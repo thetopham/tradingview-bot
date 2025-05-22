e45e0a83bcbad4f21f66eb9fb9432edfe1fb807b
thetopham <124335168+thetopham@users.noreply.github.com>
2025-05-22 00:14:51 -0600

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -1,6 +1,8 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
 import pytz
+import logging
+
 CT = pytz.timezone("America/Chicago")
 
 
