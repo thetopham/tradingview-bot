60974d74b297a4a00aad14d5be39a7784980f80d
thetopham <124335168+thetopham@users.noreply.github.com>
2026-01-07 12:03:17 -0700

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -189,7 +189,7 @@ def start_scheduler(app):
         replace_existing=True,
     )
 
-    for account in ("beta"):
+    for account in ("alpha", "beta", "gamma"):
         if account not in ACCOUNTS:
             logging.info("[APScheduler] Skipping overseer setup for missing account %s", account)
             continue
