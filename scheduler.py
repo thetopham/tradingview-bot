55b7f4fa677056ce4c056852f408b37f38f64dcd
thetopham <124335168+thetopham@users.noreply.github.com>
2026-01-07 22:37:13 -0700

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -199,21 +199,21 @@ def start_scheduler(app):
             replace_existing=True,
         )
         
-    if "gamma" in ACCOUNTS:
-        scheduler.add_job(
-            trigger_overseer,
-            CronTrigger(minute="0,5,10,15,20,25,30,35,40,45,50,55", second=15, timezone=LOCAL_TZ),
-            id="overseer_job_5m_alpha",
-            args=["gamma", "5m"],
-            replace_existing=True,
-        )
-       
     if "beta" in ACCOUNTS:
         scheduler.add_job(
             trigger_overseer,
             CronTrigger(minute="0,5,10,15,20,25,30,35,40,45,50,55", second=15, timezone=LOCAL_TZ),
             id="overseer_job_5m_beta",
             args=["beta", "5m"],
+            replace_existing=True,
+        )
+        
+    if "gamma" in ACCOUNTS:
+        scheduler.add_job(
+            trigger_overseer,
+            CronTrigger(minute="0,5,10,15,20,25,30,35,40,45,50,55", second=15, timezone=LOCAL_TZ),
+            id="overseer_job_5m_gamma",
+            args=["gamma", "5m"],
             replace_existing=True,
         )
     
