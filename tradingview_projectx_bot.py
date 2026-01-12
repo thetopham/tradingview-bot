58dd16c75a923222b254f51f10a954a13cc70149
thetopham <124335168+thetopham@users.noreply.github.com>
2026-01-12 00:14:11 -0700

    Update tradingview_projectx_bot.py

--- tradingview_projectx_bot.py
+++ tradingview_projectx_bot.py
@@ -135,6 +135,8 @@ def handle_webhook_logic(data):
                 route_label = "TEST5"
             elif acct == "practice":
                 route_label = "TEST6"
+            elif acct == "sim001":
+                route_label = "TEST7"
 
             safe_url = ai_url.split("?")[0] if ai_url else "unset"
             logging.info("[AI ROUTE] account=%s -> %s url=%s", acct, route_label, safe_url)
