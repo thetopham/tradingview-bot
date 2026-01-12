78d3f2fa32c204b78aa0fcbcaa513773203fa0c0
thetopham <124335168+thetopham@users.noreply.github.com>
2026-01-12 00:06:10 -0700

    Update config.py

--- config.py
+++ config.py
@@ -29,6 +29,7 @@ def load_config():
         'N8N_OVERSEER_URL_TEST4': os.getenv("N8N_OVERSEER_URL_TEST4"),
         'N8N_OVERSEER_URL_TEST5': os.getenv("N8N_OVERSEER_URL_TEST5"),
         'N8N_OVERSEER_URL_TEST6': os.getenv("N8N_OVERSEER_URL_TEST6"),
+        'N8N_OVERSEER_URL_TEST7': os.getenv("N8N_OVERSEER_URL_TEST7"),
         'SUPABASE_URL': os.getenv("SUPABASE_URL"),
         'SUPABASE_KEY': os.getenv("SUPABASE_KEY"),
         'WEBHOOK': os.getenv("WEBHOOK"),
