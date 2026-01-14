9cb98ea5ec9907bf59d2dbea499309d76f47e537
thetopham <124335168+thetopham@users.noreply.github.com>
2026-01-13 21:38:17 -0700

    Update api.py

--- api.py
+++ api.py
@@ -11,7 +11,7 @@ from auth import ensure_token, get_token, in_get_flat, session
 from config import load_config
 from dateutil import parser
 from supabase import create_client
-from market_regime import get_market_state
+#from market_regime import get_market_state
 
 
 config = load_config()
